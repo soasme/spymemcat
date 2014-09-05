8e470c2747ed7fa0198e6d92fc60cc1c91506e96
soasme <soasme@gmail.com>
2014-09-05 09:17:19 +0800

    Define Protocol: Memcached.

--- src/spymemcat/core.clj
+++ src/spymemcat/core.clj
@@ -28,3 +28,46 @@
   [connection-string]
   (Socket. "localhost" 11211))
 
+(defn client-factory
+  [& connection-strings])
+
+(defn send-cmd
+  "Sending cmd[1] to memcached via Socket.
+
+  [1]: See memcached protocol."
+  [client cmd])
+
+(declare error-string)
+(declare client-error-string)
+(declare server-error-string)
+
+(defprotocol Memcached
+  ; Retrieval commands (2)
+  (defn get [this key])
+  (defn gets [this key])
+
+  ; Storage commands (6)
+  (defn set [this key value]
+    "store this data")
+  (defn add [this key value]
+    "store this data, but only if the server *doesn't* already hold value for this key")
+  (defn replace [this key value]
+    "store this data, but only if the server *does* already hold data for this key")
+  (defn append [this key value]
+    "add this data to an existing key after existing data")
+  (defn prepend [this key value]
+    "add this data to an existing key before existing data")
+  (defn cas [this key value]
+    "store this data but only if no one else has updated since I last fetched it.")
+
+  ; Deletion
+  (defn delete [this key])
+
+  ; Increment/Decrement
+  (defn incr [this key step])
+  (defn decr [this key step])
+
+  ; Touch
+  (defn touch [this key exptime]
+    "update the expiration time of an existing item without fetching it")
+  )

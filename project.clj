75f517938aa43514d9cf04e8f01dbd9c30d0540d
soasme <soasme@gmail.com>
2014-09-05 08:40:49 +0800

    Remove spyglass.

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :url "http://github.com/soasme/spymemcat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [spy/spymemcached "2.8.10"]]
-  :repositories {"spy-memcached" {:url "http://files.couchbase.com/maven2/"}})
+  :dependencies [[org.clojure/clojure "1.6.0"]])

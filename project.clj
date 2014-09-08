3a6304f296c36ddfe2776b97c43d35905b14cbf6
soasme <soasme@gmail.com>
2014-09-08 15:05:03 +0800

    Add scm.

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://github.com/soasme/spymemcat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/soasme/spymemcat"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [spy/spymemcached "2.8.10"]]
   :repositories {"spy-memcached" {:url "http://files.couchbase.com/maven2/"}})
